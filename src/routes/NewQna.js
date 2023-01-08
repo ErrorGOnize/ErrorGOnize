5b88529f73c3433392f27c95da279c390a5b177c
hannahchun <80413027+hannahchun@users.noreply.github.com>
2023-01-09 01:17:08 +0900

    fix[#4] : NewQna page

--- src/routes/NewQna.js
+++ src/routes/NewQna.js
@@ -1,64 +1,60 @@
-import * as React from 'react';
-import Grid from '@mui/material/Grid';
-import TextField from '@mui/material/TextField';
-import {Box, Button, Container} from '@mui/material';
-import Tag from "../components/Common/Tag";
-import Paper from '@mui/material/Paper';
+import {
+  Box,
+  Button,
+  Container,
+  InputLabel,
+  TextField,
+  Link,
+} from "@mui/material";
+import CateDropdown from "../components/Common/CateDropdown";
+import Toggle from "../components/NewNote/Toggle";
 
 export default function NewQna() {
   return (
-    <Container>
-      <Paper elevation={0} sx={{ my: { xs: 3}}}>
-        <Box display="flex" flexDirection="row" gap={128}>
-          <Button size="small"
-              type="submit"
-              variant="outlined"
-            >
-              Back
+    <Container
+      maxWidth="md"
+      //   sx={{ border: "5px solid #f3f3f3", borderRadius: 5, mt: 2 }}
+    >
+      <Box display="flex" justifyContent="space-between" sx={{ p: 3 }}>
+        <Box display="flex" alignItems="center" gap={3}>
+          <CateDropdown />
+        </Box>
+        <Box display="flex" gap={2}>
+          <Button
+            variant="outlined"
+            color="warning"
+            disableElevation
+            onClick={() => {
+              window.history.back();
+            }}
+          >
+            취소
           </Button>
-          <Button size="small"
-              type="submit"
-              variant="outlined"
-            >
-              완료
+          <Button
+            variant="contained"
+            color="warning"
+            disableElevation
+            onClick={() => {
+              window.history.back();
+            }}
+          >
+            완료
           </Button>
         </Box>
-      </Paper>
-      <Paper variant="outlined" sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}>
-        <Tag />
-        <Grid container spacing={3}>
-          <Grid item xs={12} sm={6}>
-            <TextField fullWidth
-              required
-              id="title"
-              name="title"
-              label="제목"
-              variant="standard"
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <TextField fullWidth
-              required
-              id="content"
-              name="content"
-              label="질문 내용"
-              multiline
-              maxRows={20}
-              variant="standard"
-            />
-          </Grid>
-          <Grid item xs={12}>
-            <TextField fullWidth
-              required
-              id="code"
-              label="코드"
-              multiline
-              maxRows={10}
-              variant="standard"
-            />
-          </Grid>
-        </Grid>
-      </Paper>
+      </Box>
+
+      <Box>
+        <InputLabel sx={{ pl: 3.5 }}>제목</InputLabel>
+        <TextField fullWidth sx={{ p: 3, pt: 0.5 }} size="small" />
+        <InputLabel sx={{ pl: 3.5 }}>내용</InputLabel>
+        <TextField
+          fullWidth
+          sx={{ p: 3, pt: 0.5 }}
+          multiline
+          rows={19}
+          size="small"
+        />
+      </Box>
     </Container>
   );
 }

ea1aa7e02bcd03383b8adca0d4fbb8af33c7425d
Kuma0008 <dortli1108@gmail.com>
2023-01-09 20:12:00 +0900

    feat[#2] : fix MyPage
    - card 추가
    - 공유, 편집 버튼 추가

--- src/components/MyPage/TabPanel.js
+++ src/components/MyPage/TabPanel.js
@@ -1,29 +1,56 @@
 import PropTypes from 'prop-types';
 import Box from '@mui/material/Box';
-import Typography from '@mui/material/Typography';
+import { Grid, Button } from '@mui/material';
+import PfBtn from '../Dashboard/PfBtn';
+import Cards from "./Cards";
 
-export default function TabPanel({props}) {
-    const { children, value, index, ...other } = props;
-  
+const QuestionBar = (mode) => {
+  console.log(mode.value);
+  return (
+    <Box
+      sx={{
+        p: "10px 10px",
+        display: "flex",
+        width: "calc(60vw)",
+        border: "5px solid #f3f3f3",
+        borderRadius: 4,
+      }}
+    >
+      <PfBtn/>
+      <Button
+        variant="contained"
+        sx={{
+            margin: 0.8,
+            ml: 4,  
+            backgroundColor: "#f3f3f3",
+            fontWeight: 900,
+            fontSize: "1rem",
+            width: "calc(50vw)",
+            height: "calc(4vw)",
+            borderRadius: 4, 
+            fontFamily: "Prosto One", 
+            letterSpacing: 2,
+            color: "#d3d3d3",
+            justifyContent: "left" 
+    }}
+    >
+      나누고 싶은 { mode.value=='NOTE' ?  '생각이 있으신가요?' : '질문이 있으신가요?'}
+    </Button>
+    </Box>
+  );
+}
+
+export default function TabPanel({ mode }) {  
     return (
-      <div
-        role="tabpanel"
-        hidden={value !== index}
-        id={`simple-tabpanel-${index}`}
-        aria-labelledby={`simple-tab-${index}`}
-        {...other}
-      >
-        {value === index && (
-          <Box sx={{ p: 3 }}>
-            <Typography>{children}</Typography>
-          </Box>
-        )}
+      <div>
+        <Box >
+          <QuestionBar value={mode}/>
+          <Grid container spacing={10}> 
+            <Grid item xs={6} alignItems="center">
+              <Cards mode={mode}></Cards>
+            </Grid>
+          </Grid>
+        </Box>
       </div>
     );
 }
-
-TabPanel.propTypes = {
-    children: PropTypes.node,
-    index: PropTypes.number.isRequired,
-    value: PropTypes.number.isRequired,
-  };
--- src/routes/Mypage.js
+++ src/routes/Mypage.js
@@ -1,27 +1,89 @@
 import { useState, useEffect } from "react";
-import { Container, Box } from "@mui/material";
+import { Container, Box, Button, Stack } from "@mui/material";
 import Grid from "@mui/material/Grid";
-import Divider from "@mui/material/Divider";
 
 import Tab from "@mui/material/Tab";
 import Tabs from "@mui/material/Tabs";
 import Avatar from "@mui/material/Avatar";
-import Chips from "../components/Mypage/Chips";
-import NoteCard from "../components/Note/NoteCard";
-import QuestionBar from "../components/Dashboard/QuestionBar";
+import Chips from "../components/MyPage/Chips";
+import TabPanel from "../components/MyPage/TabPanel";
 
 export default function Mypage() {
-  const { value, setValue } = useState("note");
+  const [ tab, setTab ] = useState("NOTE");
 
-  const handleChange = (newValue) => {
-    setValue(newValue);
-    console.log(newValue);
+  const tabChange = (newValue) => {
+    setTab(newValue.target.innerText);
+    console.log(newValue.target.innerText);
   };
 
   //       <Box display="flex" flexDirection="row" sx={{ pl: 10, pr: 10 }}>
 
   return (
     <Container sx={{ pt: 7, width: "calc(75vw)" }}>
+      <Box display="flex" flexDirection="row" sx={{ pl: 10, pr: 10 }}>
+        <Grid container spacing={2}>
+          <Grid item xs={4} alignItems="center">
+            <Avatar
+              alt="name"
+              sx={{
+                bgcolor: "dodgerblue",
+                width: "50%",
+                pt: "25%",
+                pb: "25%",
+              }}
+            ></Avatar>
+          </Grid>
+          <Grid item xs={8}>
+            <Grid container>
+              <Grid item xs={9}><h3>홍길동</h3></Grid>
+              <Grid item xs={3}>
+                <Stack direction="row" alignItems="right" spacing={0.5}>
+                  <Button variant="outlined" size="small">공유</Button>
+                  <Button variant="outlined" size="small">편집</Button>
+                </Stack>
+              </Grid>
+              <p>
+              대구와 경북 동부 10개 시군에 내려졌던 초미세먼지 주의보는 오후 들어 모두 해제됐습니다.주의보는 해제됐지만 내일도 대구의 초미세먼지 농도는 매우 나쁨, 경북은 나쁨에 오후 들어 일시적으로 매우 나쁨 수준이 예상돼, 야외활동 시 주의가 필요합니다.
+              </p>
+              <p>🌐 website: www.hisnet.handong.edu</p>
+            </Grid>
+          </Grid>
+        </Grid>
+      </Box>
+      <Box display="flex" flexDirection="row" sx={{ pl: 10, pr: 10 }}>
+        <Grid container spacing={5}>
+          <Grid item xs={6} alignItems="center">
+            <Chips mode="Profile" />
+          </Grid>
+          <Grid item xs={6}>
+            <Chips mode="Interests" />
+          </Grid>
+        </Grid>
+      </Box>
+
+      <Box sx={{ width: "100%" }}>
+        <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+          <Tabs value={tab} onChange={tabChange} centered>
+            <Tab label="NOTE" value="NOTE" />
+            <Tab label="Q & A" value="Q & A" />
+          </Tabs>
+        </Box>
+      </Box>
+
+      <Box
+        display="flex"
+        flexDirection="column"
+        sx={{ pt: 3, mb: 3, pl: 10, pr: 10 }}
+      >
+        <TabPanel mode={tab}></TabPanel>
+      </Box>
+    </Container>
+  );
+}
+
+
+/*
+<Container sx={{ pt: 7, width: "calc(75vw)" }}>
       <Box display="flex" flexDirection="row" sx={{ pl: 10, pr: 10 }}>
         <Grid container spacing={2}>
           <Grid item xs={4} alignItems="center">
@@ -60,9 +122,9 @@ export default function Mypage() {
 
       <Box sx={{ width: "100%" }}>
         <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
-          <Tabs value="note" onChange={handleChange} centered>
-            <Tab label="NOTE" value="note" />
-            <Tab label="Q & A" value="qna" />
+          <Tabs value={tab} onChange={tabChange} centered>
+            <Tab label="NOTE" value="NOTE" />
+            <Tab label="Q & A" value="Q & A" />
           </Tabs>
         </Box>
       </Box>
@@ -72,11 +134,7 @@ export default function Mypage() {
         flexDirection="column"
         sx={{ pt: 3, mb: 3, pl: 10, pr: 10 }}
       >
-        <QuestionBar />
-        <NoteCard></NoteCard>
-        <NoteCard></NoteCard>
-        <NoteCard></NoteCard>
+        <TabPanel mode={tab}></TabPanel>
       </Box>
     </Container>
-  );
-}
+*/

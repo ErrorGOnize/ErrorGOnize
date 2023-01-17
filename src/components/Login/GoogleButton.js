2c572430028b36d222e2ef03eaf35562b8494685
Songdabin <thdekqls1222@naver.com>
2023-01-17 22:39:33 +0900

    style : change before-login page

--- src/components/Login/GoogleButton.js
+++ src/components/Login/GoogleButton.js
@@ -1,53 +1,50 @@
 import React, { useCallback, useEffect } from "react";
 import { GoogleLogin, GoogleLogout } from "react-google-login";
-import { gapi } from 'gapi-script';
+import { gapi } from "gapi-script";
 import { Box, Typography } from "@mui/material";
 
 const clientId =
-    '555630715109-et9a11ivurav2a87vqi58vmupuc945os.apps.googleusercontent.com';
+  "555630715109-et9a11ivurav2a87vqi58vmupuc945os.apps.googleusercontent.com";
 
 const GoogleButton = ({ onSocial }) => {
-    useEffect(() => {
-        function start() {
-            gapi.client.init({
-                clientId,
-                scope: 'email',
-            })
-        }
+  useEffect(() => {
+    function start() {
+      gapi.client.init({
+        clientId,
+        scope: "email",
+      });
+    }
 
-        gapi.load('client:auth2, start');
-    }, []);
+    gapi.load("client:auth2, start");
+  }, []);
 
-    const onSuccess = (response) => {
-        console.log(response);
-    };
+  const onSuccess = (response) => {
+    console.log(response);
+  };
 
-    const onFailure = (response) => {
-        console.log(response);
-    };
-    return (
-
-        <GoogleLogin
-            clientId={clientId}
-            // buttonText="구글계정으로 로그인하기"
-            onSuccess={onSuccess}
-            onFailure={onFailure}
+  const onFailure = (response) => {
+    console.log(response);
+  };
+  return (
+    <GoogleLogin
+      clientId={clientId}
+      // buttonText="구글계정으로 로그인하기"
+      onSuccess={onSuccess}
+      onFailure={onFailure}
+    >
+      <Box width={"calc(40vw)"}>
+        <Typography
+          fontFamily={"Prosto One"}
+          fontWeight={900}
+          fontSize="1.1rem"
+          color="#808080"
         >
-            <Box>
-                <Typography
-                    fontFamily={"Prosto One"}
-                    fontWeight={900}
-                    fontSize="1.1rem"
-                    color="#808080"
-                >
-                    구글계정으로 로그인하기
-                </Typography>
-            </Box>
-            <Box>
-                Sign in. Use your Google Account
-            </Box>
-        </GoogleLogin>
-    );
+          구글계정으로 로그인하기
+        </Typography>
+      </Box>
+      {/* <Box>Sign in. Use your Google Account</Box> */}
+    </GoogleLogin>
+  );
 };
 
 export default GoogleButton;
--- src/routes/Before.js
+++ src/routes/Before.js
@@ -1,17 +1,11 @@
-import { Box, Typography, Container } from "@mui/material";
-import UpdateIcon from "@mui/icons-material/Update";
-import CategoryIcon from "@mui/icons-material/Category";
-import NoteAltIcon from "@mui/icons-material/NoteAlt";
-import img from "../images/vision.jpeg";
-import dash1 from "../images/dash1.png";
-import note1 from "../images/note1.png";
-import qna from "../images/qna.png";
-import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import { Box, Container } from "@mui/material";
 import GoogleButton from "../components/Login/GoogleButton";
+import FirstBlock from "../components/Before/FirstBlock";
+import Image from "../images/Frame1.jpg";
 
 export default function Before() {
   return (
-    <Container sx={{ pt: 3, mb: 3 }}>
+    <Container sx={{ pt: 3, mb: 10 }}>
       <Box
         display={"flex"}
         alignItems="center"
@@ -19,229 +13,15 @@ export default function Before() {
         p={3}
         gap={10}
       >
-        <Box
-          class="img"
+        <FirstBlock />
+        <img
+          src={Image}
           style={{
-            backgroundImage: `url(${img})`,
-            backgroundSize: "cover",
             width: "calc(45vw)",
-            borderBottomLeftRadius: 20,
-            borderBottomRightRadius: 20,
-            borderTopLeftRadius: 60,
-            borderTopRightRadius: 60,
+            borderRadius: 30,
           }}
-        >
-          <Box
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              alignItems: "center",
-              backgroundColor: "rgba(0, 0, 0, 0.5)",
-              borderBottomLeftRadius: 20,
-              borderBottomRightRadius: 20,
-              borderTopLeftRadius: 60,
-              borderTopRightRadius: 60,
-            }}
-          >
-            <Typography
-              p={3}
-              fontFamily={"Prosto One"}
-              fontWeight={900}
-              fontSize="2rem"
-              color="white"
-            >
-              ErrorGOnize
-            </Typography>
-            <Typography
-              fontFamily={"Prosto One"}
-              fontWeight={900}
-              fontSize="2.5rem"
-              color="#C6C5C5"
-            >
-              Error, 복잡한 검색은 X
-            </Typography>
-            <Typography
-              fontFamily={"Prosto One"}
-              fontWeight={900}
-              fontSize="2.7rem"
-              color="#F2F2F2"
-            >
-              쉽게 찾고 해결하기
-            </Typography>
-            <KeyboardArrowDownIcon sx={{ fontSize: 50, color: "#C6C5C5" }} />
-          </Box>
-        </Box>
+        />
 
-        {/* ----------------dash---------------------- */}
-        <Box
-          sx={{
-            display: "flex",
-            alignItems: "flex-start",
-            flexDirection: "column",
-            borderRadius: 4,
-            bgcolor: "#E9E9E9",
-            p: 4,
-            width: "calc(45vw)",
-            position: "relative",
-            top: "calc(1vw)",
-            // left: "calc(0vh)",
-            zIndex: "modal",
-          }}
-        >
-          <Typography
-            fontFamily={"Prosto One"}
-            fontWeight={900}
-            fontSize="1.4rem"
-            color="#818589"
-          >
-            <UpdateIcon fontSize="large" /> 업데이트되는 새로운 질문
-          </Typography>
-          <Typography
-            fontFamily={"Prosto One"}
-            fontWeight={900}
-            fontSize="1.7rem"
-            mt={3}
-          >
-            몰랐던 Error는 확인
-            <br />
-            내가 아는 Error는 답변
-          </Typography>
-        </Box>
-        <Box
-          sx={{
-            position: "relative",
-            zIndex: "tooltip",
-            top: "calc(-40vh)",
-            left: "calc(15vw)",
-            boxShadow: 10,
-            borderRadius: 5,
-          }}
-        >
-          <img
-            className="dash1"
-            src={dash1}
-            alt="dash1"
-            style={{
-              width: "calc(25vw)",
-              borderRadius: 30,
-            }}
-          />
-        </Box>
-
-        {/* -------------------QnA--------------------- */}
-        <Box
-          display={"flex"}
-          flexDirection="column"
-          alignItems="flex-start"
-          sx={{
-            borderRadius: 5,
-            width: "calc(45vw)",
-            height: "calc(13vw)",
-            bgcolor: "#E9E9E9",
-            position: "relative",
-            top: "calc(-26vh)",
-            zIndex: "modal",
-          }}
-        >
-          <Typography
-            mt={1}
-            ml={4}
-            fontFamily={"Prosto One"}
-            fontWeight={900}
-            fontSize="1.4rem"
-            color="#818589"
-          >
-            <CategoryIcon fontSize="large" /> 카테고리로 분류된 에러
-          </Typography>
-          <Typography
-            mt={4}
-            ml={4}
-            fontFamily={"Prosto One"}
-            fontWeight={900}
-            fontSize="1.7rem"
-          >
-            쉽고 빠른 에러 검색
-          </Typography>
-        </Box>
-        <Box
-          sx={{
-            position: "relative",
-            zIndex: "tooltip",
-            top: "calc(-70vh)",
-            left: "calc(15vw)",
-            boxShadow: 10,
-            borderRadius: 5,
-          }}
-        >
-          <img
-            className="qna"
-            src={qna}
-            alt="qna"
-            style={{
-              width: "calc(25vw)",
-              borderRadius: 30,
-            }}
-          />
-        </Box>
-
-        {/* -------------------note------------------------ */}
-
-        <Box
-          display={"flex"}
-          flexDirection="column"
-          alignItems="flex-start"
-          sx={{
-            borderRadius: 5,
-            width: "calc(45vw)",
-            height: "calc(13vw)",
-            bgcolor: "#E9E9E9",
-            position: "relative",
-            top: "calc(-65vh)",
-            zIndex: "modal",
-          }}
-        >
-          <Typography
-            mt={1}
-            ml={4}
-            fontFamily={"Prosto One"}
-            fontWeight={900}
-            fontSize="1.4rem"
-            color="#818589"
-          >
-            <NoteAltIcon fontSize="large" /> 나만의 노트 작성
-          </Typography>
-          <Typography
-            mt={4}
-            ml={4}
-            fontFamily={"Prosto One"}
-            fontWeight={900}
-            fontSize="1.7rem"
-          >
-            해결된 에러를 기록하여
-            <br />
-            나만의 노트 만들기
-          </Typography>
-        </Box>
-        <Box
-          sx={{
-            position: "relative",
-            zIndex: "tooltip",
-            top: "calc(-100vh)",
-            left: "calc(15vw)",
-            boxShadow: 10,
-            borderRadius: 5,
-          }}
-        >
-          <img
-            className="note1"
-            src={note1}
-            alt="note1"
-            style={{
-              width: "calc(25vw)",
-              borderRadius: 30,
-            }}
-          />
-        </Box>
         <GoogleButton />
       </Box>
     </Container>

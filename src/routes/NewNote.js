c4730501d7c9d10e772e8cead5b8c71e09e7f320
Songdabin <thdekqls1222@naver.com>
2023-01-19 15:43:55 +0900

    feat : note add api 연결

--- src/routes/NewNote.js
+++ src/routes/NewNote.js
@@ -12,7 +12,7 @@ import CateDropdown from "../components/Common/CateDropdown";
 import Toggle from "../components/NewNote/Toggle";
 
 export default function NewNote() {
-  const [writer, setWriter] = useState("");
+  const [writer, setWriter] = useState("EGOnize");
   const [category, setCategory] = useState("");
   const [visible, setVisible] = useState(true);
   const [noteTitle, setNoteTitle] = useState("");
@@ -28,6 +28,25 @@ export default function NewNote() {
     });
   };
 
+  const getCate = (cate) => {
+    // console.log("cate", cate);
+    setCategory(cate);
+  };
+
+  const getVisi = (visi) => {
+    // console.log(visi);
+    setVisible(visi);
+  };
+
+  const handleTitle = (e) => {
+    setNoteTitle(e.target.value);
+  };
+
+  const handleContent = (e) => {
+    setNoteContent(e.target.value);
+    // console.log(e.target.value);
+  };
+
   return (
     <Container
       maxWidth="md"
@@ -35,8 +54,8 @@ export default function NewNote() {
     >
       <Box display="flex" justifyContent="space-between" sx={{ p: 3 }}>
         <Box display="flex" alignItems="center" gap={3}>
-          <CateDropdown />
-          <Toggle />
+          <CateDropdown getCate={getCate} />
+          <Toggle getVisi={getVisi} />
         </Box>
         <Box display="flex" gap={2}>
           <Button
@@ -65,7 +84,13 @@ export default function NewNote() {
 
       <Box>
         <InputLabel sx={{ pl: 3.5 }}>노트 제목</InputLabel>
-        <TextField fullWidth sx={{ p: 3, pt: 0.5 }} size="small" />
+        <TextField
+          fullWidth
+          sx={{ p: 3, pt: 0.5 }}
+          size="small"
+          value={noteTitle}
+          onChange={handleTitle}
+        />
         <InputLabel sx={{ pl: 3.5 }}>노트 내용</InputLabel>
         <TextField
           fullWidth
@@ -73,6 +98,7 @@ export default function NewNote() {
           multiline
           rows={19}
           size="small"
+          onChange={handleContent}
         />
       </Box>
     </Container>
